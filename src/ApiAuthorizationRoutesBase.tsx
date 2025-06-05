89ef72f2d20aa471419eca79d5671a9c29d95afe
mrben997 <ngduth97@gmail.com>
2025-06-05 11:39:00 +0700

    fix Route to react-route-dom

--- src/ApiAuthorizationRoutesBase.tsx
+++ src/ApiAuthorizationRoutesBase.tsx
@@ -1,6 +1,10 @@
-import React, { Component } from 'react'
-import { Route } from 'react-router'
-import { ApplicationPaths, LoginActions, LogoutActions } from 'partner-oidc-auth'
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import {
+  ApplicationPaths,
+  LoginActions,
+  LogoutActions,
+} from "partner-oidc-auth";
 
 /**
  *function this.props.loginAction(name: string) {
@@ -11,25 +15,52 @@ import { ApplicationPaths, LoginActions, LogoutActions } from 'partner-oidc-auth
  * }
  */
 export class ApiAuthorizationRoutesBase extends Component {
-  loginAction = (name: string): JSX.Element => <div>{name}</div>
-  logoutAction = (name: string): JSX.Element => <div>{name}</div>
+  loginAction = (name: string): JSX.Element => <div>{name}</div>;
+  logoutAction = (name: string): JSX.Element => <div>{name}</div>;
   render() {
     return (
       <>
-        <Route path={ApplicationPaths.Login} render={() => this.loginAction(LoginActions.Login)} />
-        <Route path={ApplicationPaths.LoginFailed} render={() => this.loginAction(LoginActions.LoginFailed)} />
-        <Route path={ApplicationPaths.LoginCallback} render={() => this.loginAction(LoginActions.LoginCallback)} />
-        <Route path={ApplicationPaths.Profile} render={() => this.loginAction(LoginActions.Profile)} />
-        <Route path={ApplicationPaths.AccessDenied} render={() => this.loginAction(LoginActions.AccessDenied)} />
-        <Route path={ApplicationPaths.Register} render={() => this.loginAction(LoginActions.Register)} />
+        <Route
+          path={ApplicationPaths.Login}
+          render={() => this.loginAction(LoginActions.Login)}
+        />
+        <Route
+          path={ApplicationPaths.LoginFailed}
+          render={() => this.loginAction(LoginActions.LoginFailed)}
+        />
+        <Route
+          path={ApplicationPaths.LoginCallback}
+          render={() => this.loginAction(LoginActions.LoginCallback)}
+        />
+        <Route
+          path={ApplicationPaths.Profile}
+          render={() => this.loginAction(LoginActions.Profile)}
+        />
+        <Route
+          path={ApplicationPaths.AccessDenied}
+          render={() => this.loginAction(LoginActions.AccessDenied)}
+        />
+        <Route
+          path={ApplicationPaths.Register}
+          render={() => this.loginAction(LoginActions.Register)}
+        />
         {/* <Route
           path={ApplicationPaths.AccessDenied}
           render={() => this.loginAction(LoginActions.AccessDenied)}
         /> */}
-        <Route path={ApplicationPaths.LogOut} render={() => this.logoutAction(LogoutActions.Logout)} />
-        <Route path={ApplicationPaths.LogOutCallback} render={() => this.logoutAction(LogoutActions.LogoutCallback)} />
-        <Route path={ApplicationPaths.LoggedOut} render={() => this.logoutAction(LogoutActions.LoggedOut)} />
+        <Route
+          path={ApplicationPaths.LogOut}
+          render={() => this.logoutAction(LogoutActions.Logout)}
+        />
+        <Route
+          path={ApplicationPaths.LogOutCallback}
+          render={() => this.logoutAction(LogoutActions.LogoutCallback)}
+        />
+        <Route
+          path={ApplicationPaths.LoggedOut}
+          render={() => this.logoutAction(LogoutActions.LoggedOut)}
+        />
       </>
-    )
+    );
   }
 }
